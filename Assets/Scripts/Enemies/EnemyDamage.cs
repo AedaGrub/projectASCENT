02c081f833959ba9902a43d6fdd121a34e74a552
Aeda <160101833+AedaGrub@users.noreply.github.com>
2024-02-27 18:46:09 +0800

    0.1.3.9
    +Player Hurt sprite
    +WIP Enemy Knockback

--- Assets/Scripts/Enemies/EnemyDamage.cs
+++ Assets/Scripts/Enemies/EnemyDamage.cs
@@ -1,20 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyDamage : MonoBehaviour
+public class enemyDamage : MonoBehaviour
 {
+    [SerializeField] private float damageAmount;
+    [SerializeField] private Vector2 knockbackDir = new Vector2(2, 0.3f);
+    [SerializeField] private float knockbackDuration = 0.1f;
+    [SerializeField] private float knockbackForce;
+    public Vector2 dir;
 
-    public int damage;
-    //public PlayerHealth playerHealth; (for whenever its made)
+    private GameObject player;
+    private playerController PlayerController;
+    private Rigidbody2D playerrb;
 
-    // Start is called before the first frame update
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
     {
-        if(collision.gameObject.tag == "Player")
+        player = GameObject.FindWithTag("Player");
+        PlayerController = player.gameObject.GetComponent<playerController>();
+        playerrb = player.gameObject.GetComponent<Rigidbody2D>();
+        dir = knockbackDir;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > player.transform.position.x)
         {
-            //playerHealth.TakeDamage(damage);
+            dir.x = knockbackDir.x * -1;
         }
+        else if (transform.position.x == player.transform.position.x)
+        {
+            if (PlayerController.isFacingRight != true)
+            {
+                dir.x = knockbackDir.x * -1;
+            }
+        }
+        else
+        {
+            dir.x = knockbackDir.x;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            //CALCULATE DAMAGE
+            //IDamageable iDamageable = col.gameObject.GetComponent<playerHealth>();
+            //if (iDamageable != null)
+            //{
+            //    iDamageable.Damage(damageAmount);
+            //}
+
+            //CALCULATE KNOCKBACK
+            //StartCoroutine(PlayerController.PlayerKnockbacked(dir, knockbackDuration));
+        }
+    }
+
+    private IEnumerator KnockbackArc()
+    {
+        Vector3 arcCenter = new Vector3(1, 0, 0);
     }
 }
---
+++ Assets/Scripts/player/playerHealth.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour , IDamageable
+{
+    public delegate void PlayerHealthChangedHandler(object source, float oldHealth, float newHealth);
+    public event PlayerHealthChangedHandler OnPlayerHealthChanged;
+
+    [SerializeField] public float maxHealth;
+    [SerializeField] private float currentHealth;
+    public float CurrentHealth => currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        float oldHealth = currentHealth;
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    public void Damage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
